ae286cb9f371b05ea8e59b18a870d5b3f457e101
Vinicius Valverde da Silva Cardoso <viniciuscardoso@uneb.br>
2026-02-25 17:26:28 -0300

    Lógica estrela

--- src/screens/Profile/Profile.tsx
+++ src/screens/Profile/Profile.tsx
@@ -1,26 +1,38 @@
-import { View, Text, Button, TouchableWithoutFeedback } from "react-native";
+import { View, Text, Button, TouchableWithoutFeedback, TouchableNativeFeedback } from "react-native";
 import React, { useState } from "react";
 import { useNavigation } from "@react-navigation/native";
 import Perfil from "../../components/Perfil/Perfil";
 import Skills from "../../components/Skills/Skills";
+import AntDesign from '@expo/vector-icons/AntDesign';
 
 export default function Profile() {
-  const [ligado,setLigado] = useState<boolean>(false)
+  const [star1,setStar1] = useState<boolean>(false)
+
+  const [star2,setStar2] = useState<boolean>(false)
+
+  const [star3,setStar3] = useState<boolean>(false)
   
-      function switchStart(){
-          return setLigado(!ligado)
+  function switchStars(){
+    setStar1(!star1)
+    if (star1){
+      setStar2(!star2)
+      if(star2){
+        setStar3(!star3)
       }
+    }
+    
+  }
+      
   
   return (
     <View>
       <Perfil nome="Minhas Habilidades" />
-      {ligado ? (<TouchableWithoutFeedback onPress={switchStart}>
-                    <Text>CLicque Aqui para ligar</Text>
-                 </TouchableWithoutFeedback>):
-            (
-            <TouchableWithoutFeedback onPress={switchStart}>
-                    <Text>CLicque Aqui para desligar</Text>
-            </TouchableWithoutFeedback>)}
+      {star1?(<TouchableWithoutFeedback onPress={switchStars}>
+        <AntDesign name="star" size={24} color="black" />
+        </TouchableWithoutFeedback>):(<TouchableWithoutFeedback onPress={switchStars}>
+        <AntDesign name="star" size={24} color="yellow" />
+        </TouchableWithoutFeedback>)}
+      
     </View>
   );
 }
